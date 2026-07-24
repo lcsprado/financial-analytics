dc9748204d716099706926a23bede3fd44709d3a
Lucas Prado <lcsh.prado@gmail.com>
2026-07-24 16:32:10 -0300

    Adicionar filtro por período nas emissões

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import FinancialDashboard from "@/components/FinancialDashboard";
+import InvoiceDateRangeFilter from "@/components/InvoiceDateRangeFilter";
 import ReceiptDateRangeFilter from "@/components/ReceiptDateRangeFilter";
 
 export default function Home() {
   return (
     <>
       <FinancialDashboard />
+      <InvoiceDateRangeFilter />
       <ReceiptDateRangeFilter />
     </>
   );
