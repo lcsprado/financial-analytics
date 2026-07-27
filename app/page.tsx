fae15939387c288ed1c642e7159bfbd9ed9a18d9
Lucas Prado <lcsh.prado@gmail.com>
2026-07-27 14:24:55 -0300

    Show print action across dashboard views

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import FinancialDashboard from "@/components/FinancialDashboard";
 import InvoiceDateRangeFilter from "@/components/InvoiceDateRangeFilter";
+import PrintButton from "@/components/PrintButton";
 import ReceiptDateRangeFilter from "@/components/ReceiptDateRangeFilter";
 
 export default function Home() {
   return (
     <>
+      <PrintButton />
       <FinancialDashboard />
       <InvoiceDateRangeFilter />
       <ReceiptDateRangeFilter />
