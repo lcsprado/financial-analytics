b4218763504c5c746917d5d3112173afac431ecf
Lucas Prado <lcsh.prado@gmail.com>
2026-07-28 14:05:27 -0300

    Ativar variação mensal no dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import ClientFilterSearchEnhancer from "@/components/ClientFilterSearchEnhancer"
 import DashboardVisualControls from "@/components/DashboardVisualControls";
 import FinancialDashboard from "@/components/FinancialDashboard";
 import InvoiceDateRangeFilter from "@/components/InvoiceDateRangeFilter";
+import MonthlyVariationEnhancer from "@/components/MonthlyVariationEnhancer";
 import PrintButton from "@/components/PrintButton";
 import ReceiptDateRangeFilter from "@/components/ReceiptDateRangeFilter";
 
@@ -12,6 +13,7 @@ export default function Home() {
       <FinancialDashboard />
       <ClientFilterSearchEnhancer />
       <DashboardVisualControls />
+      <MonthlyVariationEnhancer />
       <InvoiceDateRangeFilter />
       <ReceiptDateRangeFilter />
     </>
