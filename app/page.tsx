733dd3d075c0b5c71b187510fabfc3e47e2370dd
Lucas Prado <lcsh.prado@gmail.com>
2026-08-06 11:59:07 -0300

    Ativa separação entre previstos e recebidos

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import MonthlyVariationEnhancer from "@/components/MonthlyVariationEnhancer";
 import PrintButton from "@/components/PrintButton";
 import ReceiptClientsFallback from "@/components/ReceiptClientsFallback";
 import ReceiptDateRangeFilter from "@/components/ReceiptDateRangeFilter";
-import ReceiptForecastEnhancerV7 from "@/components/ReceiptForecastEnhancerV7";
+import ReceiptForecastEnhancerV8 from "@/components/ReceiptForecastEnhancerV8";
 import ReportSourceLabels from "@/components/ReportSourceLabels";
 import ScopedClientFilterEnhancer from "@/components/ScopedClientFilterEnhancer";
 
@@ -33,7 +33,7 @@ export default function Home() {
       <InvoiceDateRangeFilter />
       <InvoiceAnalyticsEnhancer />
       <ReceiptDateRangeFilter />
-      <ReceiptForecastEnhancerV7 />
+      <ReceiptForecastEnhancerV8 />
     </>
   );
 }
