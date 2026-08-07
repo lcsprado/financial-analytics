f2176cd61303f0fa6bb35e714d7d46f4e44c91ae
Lucas Prado <lcsh.prado@gmail.com>
2026-08-07 10:48:13 -0300

    Ativa previsão por dia habitual

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import MonthlyVariationEnhancer from "@/components/MonthlyVariationEnhancer";
 import PrintButton from "@/components/PrintButton";
 import ReceiptClientsFallback from "@/components/ReceiptClientsFallback";
 import ReceiptDateRangeFilter from "@/components/ReceiptDateRangeFilter";
-import ReceiptForecastEnhancerV9 from "@/components/ReceiptForecastEnhancerV9";
+import ReceiptForecastEnhancerV12 from "@/components/ReceiptForecastEnhancerV12";
 import ReportSourceLabels from "@/components/ReportSourceLabels";
 import ScopedClientFilterEnhancer from "@/components/ScopedClientFilterEnhancer";
 
@@ -33,7 +33,7 @@ export default function Home() {
       <InvoiceDateRangeFilter />
       <InvoiceAnalyticsEnhancer />
       <ReceiptDateRangeFilter />
-      <ReceiptForecastEnhancerV9 />
+      <ReceiptForecastEnhancerV12 />
     </>
   );
 }
