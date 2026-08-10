02b2148458230f8c9e0248b408d3e038cce63b1b
Lucas Prado <lcsh.prado@gmail.com>
2026-08-10 14:01:43 -0300

    Evita enhancers pesados fora da aba ativa

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,12 @@
-import ClientFilterInteractionFix from "@/components/ClientFilterInteractionFix";
 import ClientFilterSearchEnhancer from "@/components/ClientFilterSearchEnhancer";
-import DashboardKpiCleanup from "@/components/DashboardKpiCleanup";
-import DashboardVisualControls from "@/components/DashboardVisualControls";
-import DashboardVisualPolishV1 from "@/components/DashboardVisualPolishV1";
 import FinancialDashboard from "@/components/FinancialDashboard";
-import InvoiceAnalyticsEnhancer from "@/components/InvoiceAnalyticsEnhancer";
 import InvoiceClientCodeNormalizer from "@/components/InvoiceClientCodeNormalizer";
-import InvoiceDateRangeFilter from "@/components/InvoiceDateRangeFilter";
 import LegacyDirectorModeCleanup from "@/components/LegacyDirectorModeCleanup";
-import MonthlyVariationEnhancer from "@/components/MonthlyVariationEnhancer";
+import PerformanceScopedEnhancers from "@/components/PerformanceScopedEnhancers";
 import PrintButton from "@/components/PrintButton";
 import ReceiptClientIdentityNormalizer from "@/components/ReceiptClientIdentityNormalizer";
-import ReceiptClientLinkManager from "@/components/ReceiptClientLinkManager";
-import ReceiptClientsFallback from "@/components/ReceiptClientsFallback";
-import ReceiptDateRangeFilter from "@/components/ReceiptDateRangeFilter";
-import ReceiptForecastComparativeCleanup from "@/components/ReceiptForecastComparativeCleanup";
 import ReceiptForecastEnhancerV13 from "@/components/ReceiptForecastEnhancerV13";
-import ReceiptForecastReceivedRuleFix from "@/components/ReceiptForecastReceivedRuleFix";
-import ReceiptForecastWeekCardsPolishV15 from "@/components/ReceiptForecastWeekCardsPolishV15";
 import ReportSourceLabels from "@/components/ReportSourceLabels";
-import ScopedClientFilterEnhancerV2 from "@/components/ScopedClientFilterEnhancerV2";
 
 export default function Home() {
   return (
@@ -31,21 +18,8 @@ export default function Home() {
       <ClientFilterSearchEnhancer />
       <InvoiceClientCodeNormalizer />
       <ReceiptClientIdentityNormalizer />
-      <ReceiptClientLinkManager />
-      <ScopedClientFilterEnhancerV2 />
-      <ClientFilterInteractionFix />
-      <DashboardKpiCleanup />
-      <DashboardVisualControls />
-      <MonthlyVariationEnhancer />
-      <ReceiptClientsFallback />
-      <InvoiceDateRangeFilter />
-      <InvoiceAnalyticsEnhancer />
-      <ReceiptDateRangeFilter />
       <ReceiptForecastEnhancerV13 />
-      <ReceiptForecastReceivedRuleFix />
-      <ReceiptForecastComparativeCleanup />
-      <ReceiptForecastWeekCardsPolishV15 />
-      <DashboardVisualPolishV1 />
+      <PerformanceScopedEnhancers />
     </>
   );
 }
