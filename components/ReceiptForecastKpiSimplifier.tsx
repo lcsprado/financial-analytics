9d998145766384db49d3dd846c0b5ca432db288e
Lucas Prado <lcsh.prado@gmail.com>
2026-08-11 17:05:44 -0300

    Simplifica KPIs da previsão

---
+++ components/ReceiptForecastKpiSimplifier.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+export default function ReceiptForecastKpiSimplifier() {
+  return (
+    <style jsx global>{`
+      .receipt-forecast-active-v13 .forecast-kpis-v13 {
+        grid-template-columns: repeat(2, minmax(0, 1fr)) !important;
+      }
+
+      .receipt-forecast-active-v13 .forecast-kpis-v13 article:nth-child(3),
+      .receipt-forecast-active-v13 .forecast-kpis-v13 article:nth-child(4) {
+        display: none !important;
+      }
+
+      @media (max-width: 760px) {
+        .receipt-forecast-active-v13 .forecast-kpis-v13 {
+          grid-template-columns: 1fr !important;
+        }
+      }
+    `}</style>
+  );
+}
